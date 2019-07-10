faad40f4a3522565c1a4d3e34aaf62f5e932de5d
Patrick Connor <connorpa@mail.desy.de>
2019-07-10 15:25:05 +0200

    designing new all-in-one approach

---
+++ Alignment/OfflineValidation/bin/DMRsingle.cc
@@ -0,0 +1,14 @@
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/ini_parser.hpp>
+
+#include <experimental/filesystem>
+
+using namespace std;
+
+namespace pt = boost::property_tree;
+namespace fs = experimental::filesystem;
+
+int main (int argc, char * argv[])
+{
+
+}
