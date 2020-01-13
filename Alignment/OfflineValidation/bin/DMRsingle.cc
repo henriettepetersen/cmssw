7aa383a3232a64587b4c1aef74c44845e64dcb84
Patrick Connor <connorpa@mail.desy.de>
2020-01-14 00:10:50 +0100

    toy single & merge DMRs seems to be working, including fire & forget strategy and local testing possible

--- Alignment/OfflineValidation/bin/DMRsingle.cc
+++ Alignment/OfflineValidation/bin/DMRsingle.cc
@@ -1,18 +1,41 @@
 #include <cstdlib>
 #include <iostream>
 
-#include <boost/property_tree/ptree.hpp>
-#include <boost/property_tree/ini_parser.hpp>
+#include "exceptions.h"
+#include "toolbox.h"
+#include "Options.h"
 
-#include <experimental/filesystem>
+#include <boost/filesystem.hpp>
 
 using namespace std;
+using namespace AllInOneConfig;
 
 namespace pt = boost::property_tree;
-namespace fs = experimental::filesystem;
+namespace fs = boost::filesystem;
 
+int single (int argc, char * argv[])
+{
+    // parse the command line
+    Options options;
+    options.helper(argc, argv);
+    options.parser(argc, argv);
+
+    pt::ptree main_tree;
+    pt::read_info(options.config, main_tree);
+
+    pt::ptree alignment = main_tree.get_child("alignment");
+    pt::ptree validation = main_tree.get_child("validation");
+
+    // just dump the content, to show that it can be used
+    dump(alignment);
+    dump(validation);
+
+    return EXIT_SUCCESS; 
+}
+
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
 int main (int argc, char * argv[])
 {
-    cout << "hi from " << __FILE__ << endl;
-    return EXIT_SUCCESS;
+    return exceptions<single>(argc, argv);
 }
+#endif
--- Alignment/OfflineValidation/bin/Options.h
+++ Alignment/OfflineValidation/bin/Options.h
@@ -7,12 +7,13 @@ namespace AllInOneConfig {
 
 class Options {
 
-    boost::program_options::options_description help, desc, hide;
+    boost::program_options::options_description help, desc, hide, env;
     boost::program_options::positional_options_description pos_hide;
 
 public:
     std::string config, key;
-    bool dry, verbose, silent;
+    //std::string CMSSW_BASE, CMSSW_RELEASE_BASE, SCRAM_ARCH;
+    bool dry;
 
     Options (bool getter = false);
     void helper (int argc, char * argv[]);
